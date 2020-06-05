7fa6ba9cbf817e3f5d6ce7fd2cc0a8368294f058
Emma <mtaichi@outlook.com>
2020-06-05 08:52:53 +0100

    first commit

---
+++ js/app.js
@@ -0,0 +1,70 @@
+$(document).ready(function () {
+
+    //    $(".item-link").magnify();
+    $('.gallery-container').magnificPopup({
+        delegate: 'a',
+        type: 'image',
+        gallery: {
+            enabled: true
+        }
+    });
+    $(".nav-link").on('click', function (event) {
+        event.preventDefault();
+        const section = event.target.href
+
+
+
+        // console.log(section);
+    })
+    $(window).scroll(function () {
+        //console.log(this)
+        let pos = this.scrollY;
+        if (pos >= 466) {
+            $('.navbar').addClass('fixed-top')
+
+
+        } else {
+            $('.navbar').removeClass('fixed-top')
+        }
+
+        const posSpec = parseInt($('#special').offset().top);
+        const posContact = parseInt($('#contact').offset().top);
+        //        console.log(posSpec, posContact)
+        if (pos >= posSpec && pos <= posContact) {
+            //            console.log(pos, posSpec, posContact)
+            $('.header-link').css({
+                "position": "fixed",
+                "z-index": "999"
+            })
+
+
+
+        } else {
+            $('.header-link').css({
+                "position": "absolute"
+
+            })
+
+
+        }
+        // console.log(pos)
+    })
+
+    $('.nav-link,.header-link,.bottom-link').on('click', function (event) {
+        event.preventDefault();
+        //const link = event.target;
+        const target = $(this).attr('href');
+        const otop = document.querySelector(target).offsetTop;
+        const jtop = $(target).offset().top;
+        // console.log('otop', otop, 'jtop', jtop)
+
+
+        $("html, body").stop().animate({
+            scrollTop: jtop - 25 //120
+        }, 3000)
+
+    })
+
+
+
+})
